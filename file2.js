bbc7e1921bf13596db7565483510a1c91e54e807
NEERAJ <70850450+NeerajRegaltos@users.noreply.github.com>
2022-11-04 20:53:46 +0530

    added a function

---
+++ file2.js
@@ -0,0 +1,52 @@
+function commonElements(arr1, arr2, arr3){
+    const l1 = arr1.length;
+    const l2 = arr2.length;
+    const l3 = arr3.length;
+
+
+
+}
+commonElements([1,2,3] , [3,4,5] , [5,6,3,7]);
+
+
+// const promise = new Promise((resolve, reject) => {
+//     resolve("Some message");
+// })
+
+// promise.then((data) => {
+//     console.log(data)
+// })
+
+// const s1 = "Neeraj";
+// let s2 = "";
+
+// for (let i = s1.length - 1; i >= 0; i--) {
+//     s2 += s1[i];
+// }
+
+// console.log(s2);
+
+
+// `const anagrams = (string1, string2) => {
+//     string1 = string1.toLowerCase().replace(/[^0-9a-z]/gi, "")
+//      string2 = string2.toLowerCase().replace(/[^0-9a-z]/gi, "") 
+//      const stringSplit  = string1.split(""); 
+ 
+//      if(string1.length === string2.length){
+//          for(let ind = 0 ; ind < string2.length ; ind++){
+//              if(stringSplit.includes(string2[ind])){
+//                  stringSplit.splice(stringSplit.indexOf(string2[ind]),1);
+//              }else{
+//                  console.log(false)
+//                  return
+//              }
+//          }
+//      }else{
+//          console.log(false)
+//          return
+//      }
+ 
+//      console.log(true)
+ 
+//  }
+
